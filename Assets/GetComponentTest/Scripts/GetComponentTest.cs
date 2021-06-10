5ec8d1d67b329e82b52b3bdf595ba7cd9be96763
Anton Kalatski <kalatksi24121991@gmail.com>
2021-06-10 21:16:14 +0300

    Differences between GetComponents

---
+++ Assets/GetComponentTest/Scripts/GetComponentTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetComponentTest : MonoBehaviour
+{
+    private Transform transform;
+    private void Start()
+    {
+        GetComponentT();
+        GetComponentString();
+        GetComponentTypeOf();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponentT();
+            GetComponentString();
+            GetComponentTypeOf();
+        }
+    }
+
+    private void GetComponentT()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            var transform = GetComponent<Transform>();
+        }
+    } 
+    private void GetComponentString()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            var  transform = (Transform) GetComponent("Transform");
+        }
+    }
+    private void GetComponentTypeOf()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            var transform = (Transform) GetComponent(typeof(Transform));
+        }
+    }
+}
