6ae07a1a79c47f4d6e47c5fc5f2f343544b9eb78
Anton Kalatski <kalatksi24121991@gmail.com>
2021-06-14 21:30:08 +0300

    [Master] Optimising enemy script with await async

---
+++ Assets/TanksAIExample/Scripts/EnemyTank.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class EnemyTank : MonoBehaviour
+{
+    private Transform _player;
+    private float _speed;
+
+    public void Initialize(Transform player, float speed)
+    {
+        _speed = speed;
+        _player = player;
+        FollowPlayer();
+    }
+    private async Task FollowPlayer()
+    {
+        while(true)
+        {
+            transform.LookAt(_player.transform.position);
+            await Task.Delay(Random.Range(1, 3));
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(0, 0, _speed*Time.deltaTime);
+    }
+}
--- Assets/TanksAIExample/Scripts/TankAIBenchmark.cs
+++ Assets/TanksAIExample/Scripts/TankAIBenchmark.cs
@@ -4,34 +4,22 @@ using UnityEngine;
 
 public class TankAIBenchmark : MonoBehaviour
 {
-    [SerializeField] private Transform[] tanks;
-    [SerializeField] private GameObject tankPrefab;
+    [SerializeField] private EnemyTank[] tanks;
+    [SerializeField] private EnemyTank tankPrefab;
     [SerializeField] private GameObject playerTank;
     [SerializeField] private int numberOfTanks;
     [SerializeField] private float speed;
 
     void Start()
     {
-        speed = Random.Range(1f, 8f);
-        tanks = new Transform[numberOfTanks];
-        GameObject tank;
+        tanks = new EnemyTank[numberOfTanks];
+        EnemyTank tank;
         for (int i = 0; i < numberOfTanks; i++)
         {
             tank = Instantiate(tankPrefab);
             tank.transform.position = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
-            tanks[i] = tank.transform;
-        }
-    }
-
-    void Update()
-    {
-        foreach (Transform t in tanks)
-        {
-            if (playerTank != null)
-            {
-                t.LookAt(playerTank.transform.position);
-                t.Translate(0, 0, speed * Time.deltaTime);
-            }
+            tank.Initialize(playerTank.transform,speed);
+            tanks[i] = tank;
         }
     }
 }
